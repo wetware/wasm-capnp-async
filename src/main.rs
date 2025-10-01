8f2042c8035e837b3c256df1e428f030d677f244
mikelsr <mikelsr@pm.me>
2025-10-01 15:14:31 +0200

    Map async pipes to guest stdin/stdio from wasmer host

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
+use capnp_rpc::{RpcSystem, rpc_twoparty_capnp, twoparty};
 use std::fs;
-use std::io::Read;
+use tokio;
+use tokio_util::compat::{TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt};
 use wasmer::{Module, Store};
-use wasmer_wasix::{Pipe, WasiEnv};
+use wasmer_wasix::{Pipe, WasiEnv, types::wasi};
 
-pub fn run_wasi_wasm_file(wasm_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+use cap::{self, echo_capnp::echoer_provider};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let wasm_path = "wasm/target/wasm32-wasi/release/wasm-capnp-async.wasm";
+
+    // Set up the RPC provider.
+    let echoer_provider: echoer_provider::Client = cap::EchoerProvider::client();
+
+    let (mut host_w, guest_r) = Pipe::channel();
+    let (host_r, mut guest_w) = Pipe::channel();
+
+    let network = twoparty::VatNetwork::new(
+        // host_r.compat(),
+        // host_w.compat(),
+        host_r.compat(),
+        host_w.compat_write(),
+        rpc_twoparty_capnp::Side::Server,
+        Default::default(),
+    );
+
+    let rpc_system = RpcSystem::new(Box::new(network), Some(echoer_provider.client));
+
+    let local = tokio::task::LocalSet::new();
+
+    local
+        .run_until(async {
+            let rpc_task = tokio::task::spawn_local(rpc_system);
+            let _ = tokio::join!(rpc_task);
+        })
+        .await;
+
+    // Spawn the RPC system to run in the background.
     let wasm_bytes = fs::read(wasm_path)?;
 
     // Create a Store.
@@ -13,25 +47,17 @@ pub fn run_wasi_wasm_file(wasm_path: &str) -> Result<(), Box<dyn std::error::Err
     // Let's compile the Wasm module.
     let module = Module::new(&store, wasm_bytes)?;
 
-    let (mut stdin_w, stdin_r) = Pipe::channel();
-    let (stdout_w, mut stdout_r) = Pipe::channel();
-
     {
         // From the [docs](https://github.com/wasmerio/wasmer/blob/main/examples/wasi_pipes.rs):
         // We use a scope to make sure the runner is dropped
         // as soon as we are done with it; otherwise, it will keep the stdout pipe
         // open.
         WasiEnv::builder("capnp demo")
-            .stdin(Box::new(stdin_r))
-            .stdout(Box::new(stdout_w))
+            .stdin(Box::new(guest_r))
+            .stdout(Box::new(guest_w))
             .run_with_store(module, &mut store)?;
     }
 
     eprintln!("Ok");
-    Ok(())
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    run_wasi_wasm_file("wasm/target/wasm32-wasip1/release/wasm.wasm")?;
     Ok(())
 }
